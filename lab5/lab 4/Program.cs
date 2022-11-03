4db2680a0fc3c1da79653bb3654a095a9b082991
Nikitos10110 <schkabsran1@gmail.com>
2022-11-03 03:05:21 +0300

    Add lab5

--- lab5/lab 4/Program.cs
+++ lab5/lab 4/Program.cs
@@ -4,26 +4,7 @@
         {
         static void Main(string[] args)
         {
-            // Задание  6.3
-            //  Ввести целое число. Разложить его на простые множители. Например, число 234 это 2*3*3*13.
-
-            int numeric = int.Parse(Console.ReadLine());
-            Console.Write( "Число " + numeric + " - это ");
-
-            int nMin = 0;
-            int nMax = numeric;
-
-            do
-            {
-                nMin ++ ;
-
-                if ( nMax % nMin == 0 )
-                    Console.Write(nMin + "*");
-
-            } while (nMin < nMax);
-
-
-
+            Console.WriteLine("Привет мир!!!");
         }
     }
 }
